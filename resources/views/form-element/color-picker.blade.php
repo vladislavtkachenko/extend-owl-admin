9a39ed4612aa574f9d936f3508c495190deb3891
vladislavtkachenko <vlad.os.011235@gmail.com>
2018-10-22 16:43:30 +0300

    Update color-picker.blade.php

--- resources/views/form-element/color-picker.blade.php
+++ resources/views/form-element/color-picker.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group form-element-text {{ $errors->has($name) ? 'has-error' : '' }}">
     <label for="{{ $name }}" class="control-label">{{ $label }} @if($required)<span class="form-element-required">*</span>@endif</label>
 
-    <div id="color-picker-component" class="input-group colorpicker-component">
+    <div id="color-picker-component" class="input-group colorpicker-component js-color-picker-component">
         <input {!! $attributes !!} type="text" value="{{$value}}" class="form-control" @if($readonly) readonly @endif/>
         <span class="input-group-addon"><i></i></span>
     </div>
